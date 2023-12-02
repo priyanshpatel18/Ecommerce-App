9dbbab64b7c0e7f4f01890f01b724ae9b1dd84d1
Priyansh Patel <111214802+Priyansh-Patel-India@users.noreply.github.com>
2023-12-02 18:44:13 +0530

    Changed Cors URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://shophub-taupe.vercel.app/",
+    origin: "https://shophub-five.vercel.app/",
     credentials: true,
     methods: ["POST", "GET"],
   })
