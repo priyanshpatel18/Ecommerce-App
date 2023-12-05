3137357b7c6fa172e4c82dcfbca84c55d928e24b
Priyansh Patel <111214802+Priyansh-Patel-India@users.noreply.github.com>
2023-12-05 11:55:40 +0530

    Added Missing Files

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/userRouter.js";
+import vendorRouter from "./routes/vendorRouter.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+// Authentication
+import cookieParser from "cookie-parser";
+
+// MiddleWares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["POST", "GET"],
+  })
+);
+app.use(cookieParser());
+
+// Routes
+app.use("/products", productRouter);
+app.use("/user", userRouter);
+app.use("/vendor", vendorRouter);
+
+// Connection
+console.log(process.env.DB_URL, process.env.PORT);
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("DataBase Connected");
+    app.listen(process.env.PORT, () => console.log("Server Connected"));
+  })
+  .catch((err) => console.error(err));
+
+export default app;
